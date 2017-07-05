35a4e352df06278b29274bcb45db120e6ec231b2
pengyy <Mr.Pyy@snakedeimac.lan>
2017-07-05 15:42:17 +0800

    [add]layer的点击事件

--- FlyBird/HomeTableViewController.m
+++ FlyBird/HomeTableViewController.m
@@ -24,7 +24,7 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    
+    CGPathRef
 
 }
 
--- FlyBird/TouchLayerViewController.m
+++ FlyBird/TouchLayerViewController.m
@@ -9,7 +9,8 @@
 #import "TouchLayerViewController.h"
 
 @interface TouchLayerViewController ()
-
+@property (nonatomic, strong) CALayer *aLayer;
+@property (nonatomic, strong) CALayer *bLayer;
 @end
 
 @implementation TouchLayerViewController
@@ -17,6 +18,46 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view from its nib.
+    
+    self.view.backgroundColor = [UIColor lightGrayColor];
+    
+    self.aLayer = [CALayer layer];
+    self.aLayer.frame = CGRectMake(50, 84, 200, 200);
+    self.aLayer.backgroundColor = [UIColor whiteColor].CGColor;
+    [self.view.layer addSublayer:self.aLayer];
+    
+    
+    self.bLayer = [CALayer layer];
+    self.bLayer.frame = CGRectMake(50, 50, 100, 100);
+    self.bLayer.backgroundColor = [UIColor blueColor].CGColor;
+    [self.aLayer addSublayer:self.bLayer];
+    
+}
+
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+
+    //get touch position relative to main view
+    CGPoint point = [[touches anyObject] locationInView:self.view];
+    //convert point to the white layer's coordinates
+    point = [self.aLayer convertPoint:point fromLayer:self.view.layer];
+    //get layer using containsPoint:
+    if ([self.aLayer containsPoint:point]) {
+        //convert point to blueLayer’s coordinates
+        point = [self.bLayer convertPoint:point fromLayer:self.aLayer];
+        if ([self.bLayer containsPoint:point]) {
+            [[[UIAlertView alloc] initWithTitle:@"Inside Blue Layer"
+                                        message:nil
+                                       delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil] show];
+        } else {
+            [[[UIAlertView alloc] initWithTitle:@"Inside White Layer"
+                                        message:nil
+                                       delegate:nil
+                              cancelButtonTitle:@"OK"
+                              otherButtonTitles:nil] show];
+        }
+    }
 }
 
 - (void)didReceiveMemoryWarning {
